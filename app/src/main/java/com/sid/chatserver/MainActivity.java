38bc68af7dba7d1e0a42e8d77a6980cbfe30e43b
Sid <mathursiddharth2000@gmail.com>
2017-07-13 00:41:21 -0700

    first commit

---
+++ app/src/main/java/com/sid/chatserver/MainActivity.java
@@ -0,0 +1,318 @@
+package com.sid.chatserver;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int SocketServerPORT = 8080;
+    TextView infoIp, infoPort, chatMsg;
+    String msgLog = "";
+    List<ChatClient> userList;
+    ServerSocket serverSocket;
+    Button turnOff;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showNotification(true);
+        infoIp = (TextView) findViewById(R.id.infoip);
+        infoPort = (TextView) findViewById(R.id.infoport);
+        chatMsg = (TextView) findViewById(R.id.chatmsg);
+        turnOff = (Button)findViewById(R.id.turnoff);
+        infoIp.setText(getIpAddress());
+        userList = new ArrayList<ChatClient>();
+        ChatServerThread chatServerThread = new ChatServerThread();
+        chatServerThread.start();
+        turnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onDestroy();
+                System.exit(0);
+            }
+        });
+    }
+
+    public void showNotification(boolean status) {
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+        Resources r = getResources();
+        String chatStatus, statusDesc;
+        if(status){
+            chatStatus = "Chat Server Running";
+            statusDesc = "Secure chat server is currently running";
+        }
+        else{
+            chatStatus = "Chat Server Stopped";
+            statusDesc = "Secure chat server has been shut down";
+        }
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker(chatStatus)
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(chatStatus)
+                .setContentText(statusDesc)
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .setSmallIcon(getNotificationIcon())
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    private int getNotificationIcon() {
+        boolean useWhiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
+        return useWhiteIcon ? R.drawable.icon_silhouette : R.drawable.ic_launcher;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showNotification(false);
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void broadcastMsg(String msg) {
+        for (int i = 0; i < userList.size(); i++) {
+            userList.get(i).chatThread.sendMsg(msg);
+            //msgLog += "- send to " + userList.get(i).name + "\n";
+        }
+
+        MainActivity.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                chatMsg.setText(msgLog);
+            }
+        });
+    }
+
+    private String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "Local IP Address: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+    private class ChatServerThread extends Thread {
+
+        @Override
+        public void run() {
+            Socket socket = null;
+
+            try {
+                serverSocket = new ServerSocket(SocketServerPORT);
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        infoPort.setText("Port: "
+                                + serverSocket.getLocalPort());
+                    }
+                });
+
+                while (true) {
+                    socket = serverSocket.accept();
+                    ChatClient client = new ChatClient();
+                    userList.add(client);
+                    ConnectThread connectThread = new ConnectThread(client, socket);
+                    connectThread.start();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    private class ConnectThread extends Thread {
+
+        Socket socket;
+        ChatClient connectClient;
+        String msgToSend = "";
+
+        ConnectThread(ChatClient client, Socket socket) {
+            connectClient = client;
+            this.socket = socket;
+            client.socket = socket;
+            client.chatThread = this;
+        }
+
+        @Override
+        public void run() {
+            DataInputStream dataInputStream = null;
+            DataOutputStream dataOutputStream = null;
+
+            try {
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                String n = dataInputStream.readUTF();
+
+                connectClient.name = n;
+
+                msgLog += connectClient.name + " connected@" +
+                        connectClient.socket.getInetAddress() +
+                        ":" + connectClient.socket.getPort() + "\n";
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        chatMsg.setText(msgLog);
+                    }
+                });
+
+                dataOutputStream.writeUTF("Welcome, " + n + "!\n");
+                dataOutputStream.flush();
+
+                broadcastMsg(n + " joined our chat.\n");
+
+                while (true) {
+                    if (dataInputStream.available() > 0) {
+                        String newMsg = dataInputStream.readUTF();
+
+
+                        msgLog += n + ": " + newMsg;
+                        MainActivity.this.runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                chatMsg.setText(msgLog);
+                            }
+                        });
+
+                        broadcastMsg(n + ": " + newMsg);
+                    }
+
+                    if (!msgToSend.equals("")) {
+                        dataOutputStream.writeUTF(msgToSend);
+                        dataOutputStream.flush();
+                        msgToSend = "";
+                    }
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                userList.remove(connectClient);
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this,
+                                connectClient.name + " removed.", Toast.LENGTH_LONG).show();
+
+                        msgLog += "-- " + connectClient.name + " leaved\n";
+                        MainActivity.this.runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                chatMsg.setText(msgLog);
+                            }
+                        });
+
+                        broadcastMsg("-- " + connectClient.name + " leaved\n");
+                    }
+                });
+            }
+
+        }
+
+        private void sendMsg(String msg) {
+            msgToSend = msg;
+        }
+
+    }
+
+    class ChatClient {
+        String name;
+        Socket socket;
+        ConnectThread chatThread;
+
+    }
+}
